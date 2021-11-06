015b89fba57f48815330071eaa854aef842ba72a
Sean Kelliher <sbkelliher@icloud.com>
2021-11-06 13:10:12 -0400

    Fix syntax error, missing semi-colon.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import {fetchWeatherData} from "./app/fetches.js"
+import {fetchWeatherData} from "./app/fetches.js";
 import {configTemp, configPrcp} from "./app/formats.js";
 
 var tempChart = new Chart(
