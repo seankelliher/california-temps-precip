22013669d39de421eb2e942c33d765089cc1a23b
Sean Kelliher <sbkelliher@icloud.com>
2021-12-02 14:33:20 -0500

    Add import and function call for 'showLoadingCircles'.

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+import {showLoadingCircles} from "./app/sup-load-circles.js";
 import {formCharts} from "./app/s5-build.js";
 
 //When DOM loads, invoke function.
 document.addEventListener("DOMContentLoaded", function () {
-
+    showLoadingCircles();
     formCharts();
 });
+
+//NOTE: Fetching data from NOAA takes a few seconds.
+//Getting it from Local Storage is faster, but still requires time.
+//Therefore, I display the loading circles in both cases.
