96a9d435568a78ec9f2870fe161d12c6c72af109
Sean Kelliher <sbkelliher@icloud.com>
2022-12-06 16:50:30 -0500

    Add files to 'dist' folder.

---
+++ dist/app/s1-fetch.js
@@ -0,0 +1,102 @@
+import {hideLoadingMsg, showErrorMsg} from "./sup-notices.js";
+
+// Below fetches annual precip & days over 90F for 6 CA cities, 1970 - 2020.
+// Variables are numerous, but spare us from long, confusing fetch strings.
+
+// Variables - base URL, data set, data categories & types.
+const base = "https:// www.ncdc.noaa.gov/cdo-web/api/v2/data?";
+const set = "datasetid=GSOY&";
+const catType1 = "datacategoryid=ANNTEMP&datatypeid=DX90&";
+const catType2 = "datacategoryid=ANNPRCP&datatypeid=PRCP&";
+const catTypes = `${catType1 + catType2}`;
+
+// Variables - stations (individual and combined).
+const sd = "stationid=GHCND:USW00023188&"; // San Diego.
+const la = "stationid=GHCND:USW00023174&";
+const bfield = "stationid=GHCND:USW00023155&"; // Bakersfield.
+const fresno = "stationid=GHCND:USW00093193&";
+const stockton = "stationid=GHCND:USW00023237&";
+const eureka = "stationid=GHCND:USW00024213&";
+const stations = `${sd + la + bfield + fresno + stockton + eureka}`;
+
+// Variable - units (makes results standard, not metric).
+const units = "units=standard&";
+
+// Variable - all combined, except years and limit.
+const intro = `${base + set + catTypes + stations + units}`;
+
+// Variables - time periods (individual and combined).
+const y1970s = "startdate=1970-01-01&enddate=1979-12-31&";
+const y1980s = "startdate=1980-01-01&enddate=1989-12-31&";
+const y1990s = "startdate=1990-01-01&enddate=1999-12-31&";
+const y2000s = "startdate=2000-01-01&enddate=2009-12-31&";
+const y2010s = "startdate=2010-01-01&enddate=2019-12-31&";
+const y2020s = "startdate=2020-01-01&enddate=2020-12-31&";
+const years = [y1970s, y1980s, y1990s, y2000s, y2010s];
+
+// Variable - limit (number of results returned).
+const limit = "limit=120"; // 6 stations * 2 data sets * 10 years = 120.
+
+// Fetching for the decades 1970s, 1980s, 1990s, 2000s, and 2010s.
+async function fetchData1() {
+    try {
+        const data1 = await Promise.all(
+            years.map(function (year) {
+                return fetch(`${intro + year + limit}`, {headers: {token: process.env.NOAA_TOKEN}}).then(function (response) {
+                    if (response.ok) {
+                        return response.json();
+                    } else {
+                        return Promise.reject({
+                            message: "Error fetching data. Error code:",
+                            status: response.status
+                        });
+                    }
+                });
+            })
+        );
+        return {
+            data1
+        };
+    } catch (error) {
+        // window.console.log(error.message, error.status);
+        hideLoadingMsg();
+        showErrorMsg(error.message, error.status);
+    }
+}
+
+// "Helper" function. Delays fetch by so many milliseconds.
+function sleep(ms) {
+    return new Promise(function (resolve) {
+        setTimeout(resolve, ms);
+    });
+}
+
+// Fetching for the year 2020.
+// Done separately and with a delay to keep within NOAA's rate limits.
+async function fetchData2() {
+    try {
+        await sleep(1000);
+        const data2 = await fetch(`${intro + y2020s + limit}`, {headers: {token: process.env.NOAA_TOKEN}}).then(function (response) {
+            if (response.ok) {
+                return response.json();
+            } else {
+                return Promise.reject({
+                    message: "Error fetching data. Error code:",
+                    status: response.status
+                });
+            }
+        });
+        return {
+            data2
+        };
+    } catch (error) {
+        // window.console.log(error.message, error.status);
+        hideLoadingMsg();
+        showErrorMsg(error.message, error.status);
+    }
+}
+
+export {fetchData1, fetchData2};
+
+// NOTE FOR DEVS: If you clear both browser history AND cache while testing,
+// page will hang when you try to reload it. Quit browser and re-open to fix.
---
+++ dist/app/s3-sort.js
@@ -0,0 +1,80 @@
+import {extractResults} from "./s2-extract.js";
+
+// "Helper" function.
+// Accepts timestamp of object being evaluated. Ex., 1973-01-01T00:00:00.
+// Extracts the year. Ex., 1973.
+// Subtracts 1970 to determine where value should go in array of results.
+// For example, in the sdTemp array, the value for 1973 goes in position 3.
+function findPosition(tStamp) {
+    return tStamp.substring(0, 4) - 1970;
+}
+
+async function sortResults() {
+
+    const {results} = await extractResults();
+
+    // Placeholders - annual temp (DX90) arrays.
+    const sdTemp = new Array(50);
+    const laTemp = new Array(50);
+    const bfieldTemp = new Array(50);
+    const fresnoTemp = new Array(50);
+    const stktonTemp = new Array(50);
+    const eurekaTemp = new Array(50);
+
+    // Placeholders - annual prcp (PRCP) arrays.
+    const sdPrcp = new Array(50);
+    const laPrcp = new Array(50);
+    const bfieldPrcp = new Array(50);
+    const fresnoPrcp = new Array(50);
+    const stktonPrcp = new Array(50);
+    const eurekaPrcp = new Array(50);
+
+    // For Each function, loops through "raw data." Divides it into arrays.
+    results.forEach(function (index) {
+        if (index.datatype === "DX90") {
+            if (index.station === "GHCND:USW00023188") {
+                sdTemp[findPosition(index.date)] = index.value;
+            } else if (index.station === "GHCND:USW00023174") {
+                laTemp[findPosition(index.date)] = index.value;
+            } else if (index.station === "GHCND:USW00023155") {
+                bfieldTemp[findPosition(index.date)] = index.value;
+            } else if (index.station === "GHCND:USW00093193") {
+                fresnoTemp[findPosition(index.date)] = index.value;
+            } else if (index.station === "GHCND:USW00023237") {
+                stktonTemp[findPosition(index.date)] = index.value;
+            } else if (index.station === "GHCND:USW00024213") {
+                eurekaTemp[findPosition(index.date)] = index.value;
+            }
+        } else if (index.datatype === "PRCP") {
+            if (index.station === "GHCND:USW00023188") {
+                sdPrcp[findPosition(index.date)] = index.value;
+            } else if (index.station === "GHCND:USW00023174") {
+                laPrcp[findPosition(index.date)] = index.value;
+            } else if (index.station === "GHCND:USW00023155") {
+                bfieldPrcp[findPosition(index.date)] = index.value;
+            } else if (index.station === "GHCND:USW00093193") {
+                fresnoPrcp[findPosition(index.date)] = index.value;
+            } else if (index.station === "GHCND:USW00023237") {
+                stktonPrcp[findPosition(index.date)] = index.value;
+            } else if (index.station === "GHCND:USW00024213") {
+                eurekaPrcp[findPosition(index.date)] = index.value;
+            }
+        }
+    });
+    return {
+        bfieldPrcp,
+        bfieldTemp,
+        eurekaPrcp,
+        eurekaTemp,
+        fresnoPrcp,
+        fresnoTemp,
+        laPrcp,
+        laTemp,
+        sdPrcp,
+        sdTemp,
+        stktonPrcp,
+        stktonTemp
+    };
+}
+
+export {sortResults};
