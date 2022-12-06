1078e56b411f85aaa680a3988d2d8110ee4b6a90
Sean Kelliher <sbkelliher@icloud.com>
2022-12-06 16:27:06 -0500

    More small tweaks to server.js. Fixing errors in deploying to Heroku.

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get(/.*/, (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "index.html");
 });
 
 app.listen((process.env.PORT || 3000), () => {
