ba1e3130e0e720f37d444b3eeb8fff4852d875df
Sean Kelliher <sbkelliher@icloud.com>
2022-12-06 16:03:08 -0500

    Add basic Express sever to deploy to Heroku.

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.listen((process.env.PORT || 3000), () => {
+  console.log(`Server listening on port ${process.env.PORT} or ${port}.`)
+});
