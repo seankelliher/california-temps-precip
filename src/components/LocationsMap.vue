57b6f86b020b255200d6613911c4bd5bcb32f7ef
Sean Kelliher <sbkelliher@icloud.com>
2024-06-30 21:23:14 -0400

    Add missing alt tag to Mapbox map.

--- src/components/LocationsMap.vue
+++ src/components/LocationsMap.vue
@@ -9,7 +9,7 @@
         </div>
         <!-- Mapbox token restricted to single URL -->
         <figure class="map">
-            <img src="https://api.mapbox.com/styles/v1/mapbox/streets-v12/static/pin-s-1+0056d6(-124.16367,40.80207),pin-s-2+0056d6(-121.29078,37.9577),pin-s-3+0056d6(-119.77237,36.74773),pin-s-4+0056d6(-119.01871,35.37329),pin-s-5+0056d6(-118.24368,34.05223),pin-s-6+0056d6(-117.16472,32.71571)/-118.9815,37.4282,5.19,0/600x660@2x?access_token=pk.eyJ1Ijoic2s1NjQ2NzMiLCJhIjoiY2x5Mjl4YXp5MDl5MDJrcG9weHc3ZmViZyJ9.vvFRqD8WngjCccqL6IspVw">
+            <img src="https://api.mapbox.com/styles/v1/mapbox/streets-v12/static/pin-s-1+0056d6(-124.16367,40.80207),pin-s-2+0056d6(-121.29078,37.9577),pin-s-3+0056d6(-119.77237,36.74773),pin-s-4+0056d6(-119.01871,35.37329),pin-s-5+0056d6(-118.24368,34.05223),pin-s-6+0056d6(-117.16472,32.71571)/-118.9815,37.4282,5.19,0/600x660@2x?access_token=pk.eyJ1Ijoic2s1NjQ2NzMiLCJhIjoiY2x5Mjl4YXp5MDl5MDJrcG9weHc3ZmViZyJ9.vvFRqD8WngjCccqL6IspVw" alt="map of California">
             <figcaption>1. Eureka, 2. Stockton, 3. Fresno, 4. Bakersfield, 5. Los Angeles, 6. San Diego</figcaption>
         </figure>
     </section>
