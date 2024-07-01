d504f807667cb2efdbaf43c0446f753f47659ffa
Sean Kelliher <sbkelliher@icloud.com>
2024-06-30 21:29:26 -0400

    Small text change to 'pano' caption.

--- src/components/PanoImage.vue
+++ src/components/PanoImage.vue
@@ -13,7 +13,7 @@
         </figure>
     </div>
 
-    <div class="pano-caption">Drivers on the Cabrillo Highway in Big Sur, California. Photo: <a href="https://commons.wikimedia.org/wiki/File:Winding_road_near_the_seaside_in_Big_Sur_(Unsplash).jpg" target="_blank">Robson Hatsukami Morgan</a>.</div>
+    <div class="pano-caption">A lone driver navigates the Cabrillo Highway in Big Sur, California. Photo: <a href="https://commons.wikimedia.org/wiki/File:Winding_road_near_the_seaside_in_Big_Sur_(Unsplash).jpg" target="_blank">Robson Hatsukami Morgan</a>.</div>
 </template>
 
 <style scoped>
