41d78d1d455fd430f4c151870ce21d9ceb0521df
Sean Kelliher <sbkelliher@icloud.com>
2025-06-10 23:26:39 -0400

    Modify table styles to fix 'hydration' error.

--- src/components/TempsPrecipTable.vue
+++ src/components/TempsPrecipTable.vue
@@ -42,63 +42,79 @@ import { weatherData } from "../data/weather-data.js";
             </div>
 
             <table v-if="store.displayHeat">
-                <th>
-                    <td class="roboto-serif six">Year</td>
-                    <td class="roboto-serif six">SanDg</td>
-                    <td class="roboto-serif six">LosAn</td>
-                    <td class="roboto-serif six">Bfield</td>
-                </th>
-                <tr v-for="item in weatherData" :key="item.id">
-                    <td class="roboto-serif four">{{ item.id.substring(1,5) }}</td>
-                    <td class="roboto-serif four">{{ item.sanDiegoTemps }}</td>
-                    <td class="roboto-serif four">{{ item.losAngelosTemps }}</td>
-                    <td class="roboto-serif four">{{ item.bakersfieldTemps }}</td>
-                </tr>
+                <thead>
+                    <tr>
+                        <td class="roboto-serif six">Year</td>
+                        <td class="roboto-serif six">SanDg</td>
+                        <td class="roboto-serif six">LosAn</td>
+                        <td class="roboto-serif six">Bfield</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="item in weatherData" :key="item.id">
+                        <td class="roboto-serif four">{{ item.id.substring(1,5) }}</td>
+                        <td class="roboto-serif four">{{ item.sanDiegoTemps }}</td>
+                        <td class="roboto-serif four">{{ item.losAngelosTemps }}</td>
+                        <td class="roboto-serif four">{{ item.bakersfieldTemps }}</td>
+                    </tr>
+                </tbody>
             </table>
 
             <table v-if="store.displayHeat">
-                <th>
-                    <td class="roboto-serif six">Year</td>
-                    <td class="roboto-serif six">Fresno</td>
-                    <td class="roboto-serif six">Stock</td>
-                    <td class="roboto-serif six">Eurka</td>
-                </th>
-                <tr v-for="item in weatherData" :key="item.id">
-                    <td class="roboto-serif four">{{ item.id.substring(1,5) }}</td>
-                    <td class="roboto-serif four">{{ item.fresnoTemps }}</td>
-                    <td class="roboto-serif four">{{ item.stocktonTemps }}</td>
-                    <td class="roboto-serif four">{{ item.eurekaTemps }}</td>
-                </tr>
+                <thead>
+                    <tr>
+                        <td class="roboto-serif six">Year</td>
+                        <td class="roboto-serif six">Fresno</td>
+                        <td class="roboto-serif six">Stock</td>
+                        <td class="roboto-serif six">Eurka</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="item in weatherData" :key="item.id">
+                        <td class="roboto-serif four">{{ item.id.substring(1,5) }}</td>
+                        <td class="roboto-serif four">{{ item.fresnoTemps }}</td>
+                        <td class="roboto-serif four">{{ item.stocktonTemps }}</td>
+                        <td class="roboto-serif four">{{ item.eurekaTemps }}</td>
+                    </tr>
+                </tbody>
             </table>
 
             <table v-if="store.displayPrecip">
-                <th>
-                    <td class="roboto-serif six">Year</td>
-                    <td class="roboto-serif six">SanDg</td>
-                    <td class="roboto-serif six">LosAn</td>
-                    <td class="roboto-serif six">Bfield</td>
-                </th>
-                <tr v-for="item in weatherData" :key="item.id">
-                    <td class="roboto-serif four">{{ item.id.substring(1,5) }}</td>
-                    <td class="roboto-serif four">{{ item.sanDiegoPrecip }}</td>
-                    <td class="roboto-serif four">{{ item.losAngelosPrecip }}</td>
-                    <td class="roboto-serif four">{{ item.bakersfieldPrecip }}</td>
-                </tr>
+                <thead>
+                    <tr>
+                        <td class="roboto-serif six">Year</td>
+                        <td class="roboto-serif six">SanDg</td>
+                        <td class="roboto-serif six">LosAn</td>
+                        <td class="roboto-serif six">Bfield</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="item in weatherData" :key="item.id">
+                        <td class="roboto-serif four">{{ item.id.substring(1,5) }}</td>
+                        <td class="roboto-serif four">{{ item.sanDiegoPrecip }}</td>
+                        <td class="roboto-serif four">{{ item.losAngelosPrecip }}</td>
+                        <td class="roboto-serif four">{{ item.bakersfieldPrecip }}</td>
+                    </tr>
+                </tbody>
             </table>
 
             <table v-if="store.displayPrecip">
-                <th>
-                    <td class="roboto-serif six">Year</td>
-                    <td class="roboto-serif six">Fresno</td>
-                    <td class="roboto-serif six">Stock</td>
-                    <td class="roboto-serif six">Eurka</td>
-                </th>
-                <tr v-for="item in weatherData" :key="item.id">
-                    <td class="roboto-serif four">{{ item.id.substring(1,5) }}</td>
-                    <td class="roboto-serif four">{{ item.fresnoPrecip }}</td>
-                    <td class="roboto-serif four">{{ item.stocktonPrecip }}</td>
-                    <td class="roboto-serif four">{{ item.eurekaPrecip }}</td>
-                </tr>
+                <thead>
+                    <tr>
+                        <td class="roboto-serif six">Year</td>
+                        <td class="roboto-serif six">Fresno</td>
+                        <td class="roboto-serif six">Stock</td>
+                        <td class="roboto-serif six">Eurka</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="item in weatherData" :key="item.id">
+                        <td class="roboto-serif four">{{ item.id.substring(1,5) }}</td>
+                        <td class="roboto-serif four">{{ item.fresnoPrecip }}</td>
+                        <td class="roboto-serif four">{{ item.stocktonPrecip }}</td>
+                        <td class="roboto-serif four">{{ item.eurekaPrecip }}</td>
+                    </tr>
+                </tbody>
             </table>
         </div>
             <div class="chart-table-caption roboto-flex three">Data from the National Oceanic and Atmospheric Administration (NOAA)</div>
